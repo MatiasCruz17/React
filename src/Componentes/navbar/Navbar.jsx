360b5786c740fec65788737ef2723656a43a70a4
Matías Cruz <designerservicemc@gmail.com>
2025-02-20 03:01:31 -0300

    No funcionan los import

---
+++ src/Componentes/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import Badge from '@mui/material/Badge';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>RepuestoSyS</h2>
+            <ul>
+                <li>
+                    Aceites
+                </li>
+                <li>
+                    Accesorios
+                </li>
+            </ul>
+            <Badge badgeContent={1} color="white">
+                <AddShoppingCartIcon color="action" />
+            </Badge>
+        </div>
+    )
+}
+
+export default Navbar
